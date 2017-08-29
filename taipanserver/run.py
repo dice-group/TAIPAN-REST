efd919e24c75b8fd04a034c9af7c596a5088fa14
Ivan Ermilov <earthquakesan@gmail.com>
2017-08-29 15:05:49 +0200

    sml mapping generation added as REST endpoint

--- taipanserver/run.py
+++ taipanserver/run.py
@@ -5,6 +5,8 @@ from flask import request, abort, jsonify
 from taipan.generictable import GenericTable
 from taipan.ml.subjectcolumn.scidentifier import SCIdentifier
 from taipan.recommender.properties.lov import get_table_properties
+from taipan.sml.taipantosml import TaipanToSML
+
 
 app = Flask(__name__)
 
@@ -16,7 +18,6 @@ def identify_subject_column():
     table = GenericTable("stub", _id=None, csv_string=table_string)
     table.init()
     SCIDENTIFIER = SCIdentifier()
-    #Returns nothing, needs tuning!
     subject_column = SCIDENTIFIER.identify_subject_column(table)
     return jsonify({'subject_column': subject_column}), 200
 
@@ -29,9 +30,23 @@ def recommend_properties():
     table = GenericTable("stub", _id=None, csv_string=table_string)
     table.init()
     properties = get_table_properties(table)
-    print(properties)
     return jsonify(properties), 200
 
+
+@app.route('/table/api/v1.0/generate_sml_mapping', methods=['POST'])
+def generate_sml_mapping():
+    if not request.data:
+        abort(400)
+    subject_namespace = request.headers["X-Subject-Namespace"]
+    table_string = request.data.decode("utf-8")
+    table = GenericTable("stub", _id=None, csv_string=table_string)
+    table.init()
+    sc_ident = SCIdentifier()
+    subject_column = sc_ident.identify_subject_column(table)
+    property_mappings = get_table_properties(table)
+    sml_mapping = TaipanToSML(property_mappings, subject_namespace, subject_column[0])
+    print(sml_mapping.get_mapping())
+    return sml_mapping.get_mapping(), 200
 
 
 if __name__ == '__main__':
